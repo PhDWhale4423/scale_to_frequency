5ce86e652c3479caff944c16a6e3414b0d01cb68
Ichiro Ikari <ikaring.w.steve@gmail.com>
2018-09-23 06:41:21 +0900

    なんかできた

---
+++ scale_to_frequency.py
@@ -0,0 +1,36 @@
+import re
+
+
+def scale_to_frequency(scale):
+    pattern = r"[A-G]#?[0-9]"
+    matchObj = re.match(pattern, scale)
+    if not matchObj:
+        print("Illigal scale (1)")
+        exit()
+
+    # B# and E# is not exist
+    pattern = "[BE]#[0-9]"
+    matchObj = re.match(pattern,    scale)
+    if matchObj:
+        print("Illigal scale (2)")
+        exit()
+    # A#0 -> 0
+    if scale[0] == 'A' or scale[0] == 'B':
+        f = 27.5*(2**int(scale[-1]))
+    else:
+        f = 27.5*(2**(int(scale[-1])-1))
+
+    # A#0 -> A#
+    pitch_name = ['A','A#','B','C','C#','D','D#','E','F','F#','G','G#']
+    n = pitch_name.index(scale[0:-1])
+    f = f*((2**(1/12))**n)
+
+    return f
+    
+
+if __name__ == "__main__":
+    s = input("Input Scale (\"q\":exit) : ")
+    while(s != "q"):
+        f = scale_to_frequency(s)
+        print("frequency: " + str(f) + "(Hz)")
+        s = input("Input Scale (\"q\":exit) : ")
